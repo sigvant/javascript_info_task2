8aeb3816498e7f74ab3adff37fc08e60f09da2a6
sigvant <bnsviv@gmail.com>
2021-02-28 17:06:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Working with variables task
+
+let name = "John";
+let admin;
+
+admin = name;
+
+alert(admin);
+
+// Giving The Right Name Task
+
+let solarSystemThirdPlanet = "Earth";
+let ourPlanetName = "Earth";
+
+let currentVisitorName = "Guest";
+let currentUserName = "Guest";
+
+//Uppercase CONST?
+
+const BIRTHDAY = '18.04.1982'; // make uppercase?
+
+const AGE = someCode(BIRTHDAY); // make uppercase?
+
+// uppercase for birthday because it is hard coded
+// lowercase for age because it is calculated
+
